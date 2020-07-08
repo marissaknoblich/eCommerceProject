33fc11fee1d6a498638074b1c7c51280f6db3c2d
marissa_k <maknob92@gmail.com>
2020-07-08 11:52:13 -0400

    Add Security Config

--- src/main/java/com/tts/eCommerce/configuration/SecurityConfiguration.java
+++ src/main/java/com/tts/eCommerce/configuration/SecurityConfiguration.java
@@ -1,5 +1,10 @@
 package com.tts.eCommerce.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
 }
