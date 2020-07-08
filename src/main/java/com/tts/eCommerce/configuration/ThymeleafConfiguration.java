efac933123ecbbfde932bfc0ad5ee402b5270ae5
marissa_k <maknob92@gmail.com>
2020-07-08 11:51:47 -0400

    Add Thymeleaf Config

--- src/main/java/com/tts/eCommerce/configuration/ThymeleafConfiguration.java
+++ src/main/java/com/tts/eCommerce/configuration/ThymeleafConfiguration.java
@@ -1,5 +1,13 @@
 package com.tts.eCommerce.configuration;
 
-public class ThymeleafConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
+@Configuration
+public class ThymeleafConfiguration {
+  @Bean
+  public SpringSecurityDialect springSecurityDialect() {
+    return new SpringSecurityDialect();
+  }
 }
