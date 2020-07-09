0349a39ff2a08b74d51009f0dfa3330be3874c60
marissa_k <maknob92@gmail.com>
2020-07-09 10:59:24 -0400

    Updated Repository

--- src/main/java/com/tts/eCommerce/repository/OrderRepository.java
+++ src/main/java/com/tts/eCommerce/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.tts.eCommerce.repository;
 
-import javax.persistence.criteria.Order;
-
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import com.tts.eCommerce.model.Order;
+
+@Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
+	public Order findByAll();
 
 }
--- src/main/java/com/tts/eCommerce/repository/ProductRepository.java
+++ src/main/java/com/tts/eCommerce/repository/ProductRepository.java
@@ -1,9 +1,25 @@
 package com.tts.eCommerce.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tts.eCommerce.model.Product;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
+	public List<Product> findAll();
+	public Optional<Product> findById(Long id);
+	public List<Product> findByBrand(String brand);
+	public List<Product> findByCatergory(String category);
+	public List<Product> findByBrandAndCategory(String brand, String category);
+	
+@Query("SELECT DISTINCT product.brand FROM Product product")
+public List<String> findDistinctBrands();
 
+@Query("SELECT DISTINCT product.category FROM Product product")
+public List<String> findDistinctCategories();
 }
