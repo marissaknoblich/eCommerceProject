ab3201b6ec01ec0856065b23c21c6c6c6d9faa8a
shalon <shalonevans7@gmail.com>
2020-07-16 20:55:05 -0700

    Refactor order service to purchase service

--- src/main/java/com/tts/eCommerce/service/OrderService.java
+++
@@ -1,9 +0,0 @@
-package com.tts.eCommerce.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class OrderService {
-
-
-}
---
+++ src/main/java/com/tts/eCommerce/service/PurchaseService.java
@@ -0,0 +1,13 @@
+package com.tts.eCommerce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tts.eCommerce.repository.PurchaseRepository;
+
+@Service
+public class PurchaseService {
+	@Autowired
+	private PurchaseRepository purchaseRepository;
+
+}
