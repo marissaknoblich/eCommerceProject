b2147560a94e95a5447a9d04caa8131240fda9e8
shalon <shalonevans7@gmail.com>
2020-07-16 20:48:15 -0700

    Update product service

--- src/main/java/com/tts/eCommerce/service/ProductService.java
+++ src/main/java/com/tts/eCommerce/service/ProductService.java
@@ -2,6 +2,7 @@ package com.tts.eCommerce.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,39 +10,45 @@ import com.tts.eCommerce.model.Product;
 import com.tts.eCommerce.repository.ProductRepository;
 
 public class ProductService {
-	
-	
-@Autowired
+
+	@Autowired
 	private ProductRepository productRepository;
+
 	public List<Product> findAll() {
-	  return productRepository.findAll();
+		return productRepository.findAll();
 	}
-	public Product findProductById(long id) {
-	  return productRepository.findProductById(id);
+
+	public Optional<Product> findProductById(long id) {
+		return productRepository.findById(id);
 	}
+
 	public List<String> findBrands() {
-	  return productRepository.findDistinctBrands();
+		return productRepository.findDistinctBrands();
 	}
+
 	public List<String> findCategories() {
-	  return productRepository.findDistinctCategories();
+		return productRepository.findDistinctCategories();
 	}
+
 	public void save(Product product) {
-	  productRepository.save(product);
+		productRepository.save(product);
 	}
+
 	public void deleteById(long id) {
-	  productRepository.deleteById(id);
+		productRepository.deleteById(id);
 	}
+
 	public List<Product> findByBrandAndOrCategory(String brand, String category) {
-	  List<Product> products = new ArrayList<>();
-	  if(category == null && brand == null) {
-	    products = productRepository.findAll();
-	  } else if(category == null) {
-	    products = productRepository.findByBrand(brand);
-	  } else if(brand == null) {
-	    products = productRepository.findByCatergory(category);
-	  } else {
-	    products = productRepository.findByBrandAndCategory(brand, category);
-	  }
-	  return products;
+		List<Product> products = new ArrayList<>();
+		if (category == null && brand == null) {
+			products = productRepository.findAll();
+		} else if (category == null) {
+			products = productRepository.findByBrand(brand);
+		} else if (brand == null) {
+			products = productRepository.findByCatergory(category);
+		} else {
+			products = productRepository.findByBrandAndCategory(brand, category);
+		}
+		return products;
 	}
 }
