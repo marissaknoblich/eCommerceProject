8026ebbaac4ce9837403028506302e432da3bdfe
shalon <shalonevans7@gmail.com>
2020-07-16 20:45:48 -0700

    Update cart model

--- src/main/java/com/tts/eCommerce/model/Cart.java
+++ src/main/java/com/tts/eCommerce/model/Cart.java
@@ -1,5 +1,8 @@
 package com.tts.eCommerce.model;
 
+import java.util.HashMap;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,7 +11,38 @@ import javax.persistence.Id;
 @Entity
 public class Cart {
 
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private List<HashMap<Product, Integer>> cartItems;
+	private Double subtotal;
+
+	public Cart() {}
+
+	public Cart(List<HashMap<Product, Integer>> cartItems, Double subtotal) {
+		super();
+		this.cartItems = cartItems;
+		this.subtotal = subtotal;
+	}
+
+	public List<HashMap<Product, Integer>> getCartItems() {
+		return cartItems;
+	}
+
+	public void setCartItems(List<HashMap<Product, Integer>> cartItems) {
+		this.cartItems = cartItems;
+	}
+
+	public Double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(Double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
 }
