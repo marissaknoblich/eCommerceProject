0c69f9ca4efca5b9679e15cf950c3a4055bf36ba
shalon <shalonevans7@gmail.com>
2020-07-18 19:29:41 -0700

    wq git status
    git status
    w\wq

--- src/main/java/com/tts/eCommerce/service/CartService.java
+++ src/main/java/com/tts/eCommerce/service/CartService.java
@@ -1,8 +1,91 @@
 package com.tts.eCommerce.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.tts.eCommerce.model.Product;
+import com.tts.eCommerce.repository.ProductRepository;
 
 @Service
 public class CartService {
+	
+
+	 public ProductRepository getProductRepository() {
+		return productRepository;
+	}
+
+	public void setProductRepository(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	@Autowired
+	private ProductRepository productRepository;
+	 
+	 public List<Product> findAll() {
+			return productRepository.findAll();
+		}
+
+	public Optional<Product> findById(Long id) {
+		return productRepository.findById(id);
+	}
+
+	public List<Product> findByBrand(String brand) {
+		return productRepository.findByBrand(brand);
+	}
+
+	public List<Product> findByCatergory(String category) {
+		return productRepository.findByCatergory(category);
+	}
+
+	public List<Product> findByBrandAndCategory(String brand, String category) {
+		return productRepository.findByBrandAndCategory(brand, category);
+	}
+
+	public List<String> findDistinctBrands() {
+		return productRepository.findDistinctBrands();
+	}
+
+	public List<String> findDistinctCategories() {
+		return productRepository.findDistinctCategories();
+	}
+
+	public <S extends Product> S save(S entity) {
+		return productRepository.save(entity);
+	}
+
+	public <S extends Product> Iterable<S> saveAll(Iterable<S> entities) {
+		return productRepository.saveAll(entities);
+	}
+
+	public boolean existsById(Long id) {
+		return productRepository.existsById(id);
+	}
+
+	public Iterable<Product> findAllById(Iterable<Long> ids) {
+		return productRepository.findAllById(ids);
+	}
+
+	public long count() {
+		return productRepository.count();
+	}
+
+	public void deleteById(Long id) {
+		productRepository.deleteById(id);
+	}
+
+	public void delete(Product entity) {
+		productRepository.delete(entity);
+	}
+
+	public void deleteAll(Iterable<? extends Product> entities) {
+		productRepository.deleteAll(entities);
+	}
+
+	public void deleteAll() {
+		productRepository.deleteAll();
+	}
 
 }
--- src/main/java/com/tts/eCommerce/service/PurchaseService.java
+++ src/main/java/com/tts/eCommerce/service/PurchaseService.java
@@ -1,13 +1,55 @@
 package com.tts.eCommerce.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import com.tts.eCommerce.model.Purchase;
 import com.tts.eCommerce.repository.PurchaseRepository;
 
 @Service
 public class PurchaseService {
+	
 	@Autowired
-	private PurchaseRepository purchaseRepository;
-
+	private PurchaseRepository purchaseRepository; public PurchaseRepository getPurchaseRepository() {
+		return purchaseRepository;
+	}
+	public void setPurchaseRepository(PurchaseRepository purchaseRepository) {
+		this.purchaseRepository = purchaseRepository;
 }
+	public <S extends Purchase> S save(S entity) {
+		return purchaseRepository.save(entity);
+	}
+	public <S extends Purchase> Iterable<S> saveAll(Iterable<S> entities) {
+		return purchaseRepository.saveAll(entities);
+	}
+	public Optional<Purchase> findById(Long id) {
+		return purchaseRepository.findById(id);
+	}
+	public boolean existsById(Long id) {
+		return purchaseRepository.existsById(id);
+	}
+	public Iterable<Purchase> findAll() {
+		return purchaseRepository.findAll();
+	}
+	public Iterable<Purchase> findAllById(Iterable<Long> ids) {
+		return purchaseRepository.findAllById(ids);
+	}
+	public long count() {
+		return purchaseRepository.count();
+	}
+	public void deleteById(Long id) {
+		purchaseRepository.deleteById(id);
+	}
+	public void delete(Purchase entity) {
+		purchaseRepository.delete(entity);
+	}
+	public void deleteAll(Iterable<? extends Purchase> entities) {
+		purchaseRepository.deleteAll(entities);
+	}
+	public void deleteAll() {
+		purchaseRepository.deleteAll();
+	}
+	}
